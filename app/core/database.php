9e030a2fe8a8431522e2b89cb32ecd00e9dd598f
latifi777 <abdelalilatifi3@gmail.com>
2025-02-07 12:40:27 +0100

    minor changes

--- app/core/database.php
+++ app/core/database.php
@@ -24,7 +24,6 @@ class Database {
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                     PDO::ATTR_EMULATE_PREPARES => false
                 ]);
-                echo 'connected';
             } catch (PDOException $e) {
                 throw new \Exception("Database connection failed: " . $e->getMessage());
             }
